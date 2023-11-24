be719432cafe0fada275b910df27ca699af04126
ikhsanazis <mxnzis@gmail.com>
2023-11-24 19:32:09 +0700

    adding, updating, and deleting note

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,36 @@
+const {
+  addNotesHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteById,
+  deleteNnoteById,
+} = require('./handler');
+
 const routes = [
   {
+    method: 'POST',
+    path: '/notes',
+    handler: addNotesHandler,
+  },
+  {
     method: 'GET',
-    path: '/',
-    handler: () => {
-      return 'test';
-    },
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteById,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNnoteById,
   },
 ];
 
