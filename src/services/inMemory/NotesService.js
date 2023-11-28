91e917e4bb4d0302db8d372097807cd7d9d7a380
ikhsanazis <mxnzis@gmail.com>
2023-11-28 21:03:27 +0700

    add Noteservice

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,17 @@
+const nanoid = require('nanoid');
+
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+
+  addNote({}) {}
+
+  getNotes() {}
+
+  getNoteByID(id) {}
+
+  editNoteById(id, { title, body, tags }) {}
+
+  deleteNoteById(id) {}
+}
