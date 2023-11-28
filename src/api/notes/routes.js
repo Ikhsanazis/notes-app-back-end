52f85bfc5ad4c2683490f230d6e99437ca868a4e
ikhsanazis <mxnzis@gmail.com>
2023-11-28 21:09:15 +0700

    update routes

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -1,37 +1,67 @@
-const {
-  addNotesHandler,
-  getAllNotesHandler,
-  getNoteByIdHandler,
-  editNoteById,
-  deleteNnoteById,
-} = require('./handler');
-
-const routes = [
+const routes = (handler) => [
   {
     method: 'POST',
     path: '/notes',
-    handler: addNotesHandler,
+    handler: handler.postNotesHandler,
   },
   {
     method: 'GET',
     path: '/notes',
-    handler: getAllNotesHandler,
+    handler: handler.getAllNotesHandler,
   },
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: getNoteByIdHandler,
+    handler: handler.getNoteByIdHandler,
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
-    handler: editNoteById,
+    handler: handler.putNoteById,
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
-    handler: deleteNnoteById,
+    handler: handler.deleteNoteById,
   },
 ];
 
 module.exports = routes;
+
+// const {
+//   addNotesHandler,
+//   getAllNotesHandler,
+//   getNoteByIdHandler,
+//   editNoteById,
+//   deleteNnoteById,
+// } = require('./handler');
+
+// const routes = [
+//   {
+//     method: 'POST',
+//     path: '/notes',
+//     handler: addNotesHandler,
+//   },
+//   {
+//     method: 'GET',
+//     path: '/notes',
+//     handler: getAllNotesHandler,
+//   },
+//   {
+//     method: 'GET',
+//     path: '/notes/{id}',
+//     handler: getNoteByIdHandler,
+//   },
+//   {
+//     method: 'PUT',
+//     path: '/notes/{id}',
+//     handler: editNoteById,
+//   },
+//   {
+//     method: 'DELETE',
+//     path: '/notes/{id}',
+//     handler: deleteNnoteById,
+//   },
+// ];
+
+// module.exports = routes;
